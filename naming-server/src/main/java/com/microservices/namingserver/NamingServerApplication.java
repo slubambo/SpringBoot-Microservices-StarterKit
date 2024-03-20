594653951523272687a748c663cb3bb00db2fc58
slubambo <slubambo57@gmail.com>
2024-03-20 16:21:17 +0300

    🚧 Refactoring packages

--- naming-server/src/main/java/com/microservices/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/microservices/namingserver/NamingServerApplication.java
@@ -1,4 +1,4 @@
-package mis.services.namingserver;
+package com.microservices.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
