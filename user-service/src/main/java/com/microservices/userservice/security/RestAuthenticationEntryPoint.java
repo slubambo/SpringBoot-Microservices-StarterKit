be7584a897124d0c533d09cf3488172edf5efc78
slubambo <slubambo57@gmail.com>
2024-03-21 16:46:15 +0300

    🚧 RestAuthenticationEntryPoint and other auth2 handlers

---
+++ user-service/src/main/java/com/microservices/userservice/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.microservices.userservice.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint.class);
+
+	@Override
+	public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			AuthenticationException e) throws IOException, ServletException {
+		logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+		httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getLocalizedMessage());
+	}
+}
