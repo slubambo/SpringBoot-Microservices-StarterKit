939522a51c48c78231a91d44d58b2cf1e6a12ed7
slubambo <slubambo57@gmail.com>
2024-03-22 15:02:57 +0300

    🚧 Setting default user model constructor

--- user-service/src/main/java/com/microservices/userservice/model/User.java
+++ user-service/src/main/java/com/microservices/userservice/model/User.java
@@ -55,6 +55,10 @@ public class User extends DateAudit {
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
+	public User() {
+		super();
+	}
+
 	public User(String name, String username, String email, String password) {
 		this.name = name;
 		this.username = username;
