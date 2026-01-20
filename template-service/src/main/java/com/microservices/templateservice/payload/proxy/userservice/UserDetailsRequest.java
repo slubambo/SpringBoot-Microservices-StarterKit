7e859113c1b28d012de9fb2017b1abe0cdcaf65e
Simon Fred Lubambo <slubambo57@gmail.com>
2026-01-20 19:50:38 +0300

    Remove Lombok and add explicit methods in UserDetailsRequest
    Replaces Lombok annotations with explicit constructors, getters, and setters in UserDetailsRequest.java for improved clarity and reduced dependency on Lombok.

--- template-service/src/main/java/com/microservices/templateservice/payload/proxy/userservice/UserDetailsRequest.java
+++ template-service/src/main/java/com/microservices/templateservice/payload/proxy/userservice/UserDetailsRequest.java
@@ -1,16 +1,32 @@
 package com.microservices.templateservice.payload.proxy.userservice;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@Getter
-@Setter
 public class UserDetailsRequest {
 
 	private Long id;
 
 	private String usernameOrEmail;
 
+	public UserDetailsRequest() {
+	}
+
+	public UserDetailsRequest(Long id, String usernameOrEmail) {
+		this.id = id;
+		this.usernameOrEmail = usernameOrEmail;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsernameOrEmail() {
+		return usernameOrEmail;
+	}
+
+	public void setUsernameOrEmail(String usernameOrEmail) {
+		this.usernameOrEmail = usernameOrEmail;
+	}
 }
