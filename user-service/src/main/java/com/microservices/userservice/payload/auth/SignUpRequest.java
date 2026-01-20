17024554d56d46c704fdf530902bc5d2da805729
Simon Fred Lubambo <slubambo57@gmail.com>
2026-01-20 19:39:59 +0300

    Remove Lombok and add explicit getters/setters in SignUpRequest
    Replaces Lombok annotations with explicit getter and setter methods in the SignUpRequest class for better clarity and to remove Lombok dependency.

--- user-service/src/main/java/com/microservices/userservice/payload/auth/SignUpRequest.java
+++ user-service/src/main/java/com/microservices/userservice/payload/auth/SignUpRequest.java
@@ -1,13 +1,9 @@
 package com.microservices.userservice.payload.auth;
 
-import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class SignUpRequest {
 
 	@NotBlank
@@ -26,5 +22,42 @@ public class SignUpRequest {
 	@NotBlank
 	@Size(min = 6, max = 20)
 	private String password;
+
+	public SignUpRequest() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 
 }
