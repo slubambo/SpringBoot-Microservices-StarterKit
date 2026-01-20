86129d9697580bfa43f5322d362e1c9c3f5f51f1
Simon Fred Lubambo <slubambo57@gmail.com>
2026-01-20 19:39:41 +0300

    Remove Lombok getters/setters and add explicit methods
    Replaces Lombok @Getter and @Setter annotations in SessionUser with explicit getter and setter methods for all fields. This change increases code clarity and may help with IDE support or debugging.

--- template-service/src/main/java/com/microservices/templateservice/config/SessionUser.java
+++ template-service/src/main/java/com/microservices/templateservice/config/SessionUser.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
-@Getter
-@Setter
 @NoArgsConstructor
 public class SessionUser {
 
@@ -29,6 +27,54 @@ public class SessionUser {
 	public String toString() {
 		return "UserTokenHeader [id=" + id + ", name=" + name + ", username=" + username + ", userType=" + userType
 				+ ", authorities=" + authorities + ", permissions=" + permissions + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public Collection<?> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Collection<?> authorities) {
+		this.authorities = authorities;
+	}
+
+	public Collection<?> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(Collection<?> permissions) {
+		this.permissions = permissions;
 	}
 
 }
