7254fd1e5682ff91b24950c51e1d68dec02c85a8
Simon Fred Lubambo <slubambo57@gmail.com>
2026-01-20 19:40:19 +0300

    Remove Lombok annotations from UserDetailsRequest
    Replaces Lombok-generated getters, setters, and constructor in UserDetailsRequest with explicit method implementations. This change may improve clarity and reduce dependency on Lombok.

--- user-service/src/main/java/com/microservices/userservice/payload/user/UserDetailsRequest.java
+++ user-service/src/main/java/com/microservices/userservice/payload/user/UserDetailsRequest.java
@@ -1,16 +1,29 @@
 package com.microservices.userservice.payload.user;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@Getter
-@Setter
 public class UserDetailsRequest {
 
 	private Long id;
 
 	private String usernameOrEmail;
+
+	public UserDetailsRequest() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsernameOrEmail() {
+		return usernameOrEmail;
+	}
+
+	public void setUsernameOrEmail(String usernameOrEmail) {
+		this.usernameOrEmail = usernameOrEmail;
+	}
 
 }
