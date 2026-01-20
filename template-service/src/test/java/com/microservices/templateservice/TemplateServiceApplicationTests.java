69f3d9eef83f47a7c89fa77a7e68016752d6f5b7
Simon Fred Lubambo <slubambo57@gmail.com>
2026-01-20 19:31:28 +0300

    Add basic context load test for TemplateService
    Introduces a JUnit test to verify that the Spring Boot application context loads successfully for TemplateServiceApplication.

---
+++ template-service/src/test/java/com/microservices/templateservice/TemplateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.templateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TemplateServiceApplication.class)
+class TemplateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
